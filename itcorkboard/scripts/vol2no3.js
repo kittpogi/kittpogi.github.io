ce0e89a35b2d66a5e0b874c11882a5d0a55f0322
Francis Santos <30050432@westernsydney.edu.au>
2019-07-29 12:09:36 +1000

    wsu commit 20190729
    fixed case-sensitive URLs as per rqs' comments

--- itcorkboard/scripts/vol2no3.js
+++ itcorkboard/scripts/vol2no3.js
@@ -2,6 +2,6 @@
 
 function printLinks(){
 
-document.write('<table width="100%" border="0" cellspacing="0" cellpadding="1"><tr align="center"><td><font face="Verdana, Geneva, sans serif" size="1"><b><a href="../../vol2/no3/updates.html">up 2 date</a> | <a href="../../vol2/no3/events.html">events</a> | <a href="../../vol2/no3/technook.html">tech nook</a> | <a href="../../vol2/no3/viewpoint.html">view point</a> | <a href="../../vol2/no3/../../archives.html">archives</a> | <a href="../../ISTP/etcetera7/index.html">etcetera</a> | <a href="../../vol2/no3/links.html">links</a> | <a href="../../vol2/no3/onkeyboard.html">onKeyboard</a></b></font></td></tr></table>');
+document.write('<table width="100%" border="0" cellspacing="0" cellpadding="1"><tr align="center"><td><font face="Verdana, Geneva, sans serif" size="1"><b><a href="../../vol2/no3/updates.html">up 2 date</a> | <a href="../../vol2/no3/events.html">events</a> | <a href="../../vol2/no3/technook.html">tech nook</a> | <a href="../../vol2/no3/viewpoint.html">view point</a> | <a href="../../vol2/no3/../../archives.html">archives</a> | <a href="../../istp/etcetera7/index.html">etcetera</a> | <a href="../../vol2/no3/links.html">links</a> | <a href="../../vol2/no3/onkeyboard.html">onKeyboard</a></b></font></td></tr></table>');
 
 }
--- itcorkboard/scripts/vol3no1.js
+++ itcorkboard/scripts/vol3no1.js
@@ -2,6 +2,6 @@
 
 function printLinks(){
 
-document.write('<table width="100%" border="0" cellspacing="0" cellpadding="1"><tr align="center"><td><font face="Verdana, Geneva, sans serif" size="1"><b><a href="../../vol3/no1/updates.html">up 2 date</a> | <a href="../../vol3/no1/events.html">events</a> | <a href="../../vol3/no1/technook.html">tech nook</a> | <a href="../../vol3/no1/viewpoint.html">view point</a> | <a href="../../archives.html">archives</a> | <a href="../../ISTP/etcetera8/index.html">etcetera</a> | <a href="../../vol3/no1/links.html">links</a> | <a href="../../vol3/no1/onkeyboard.html">onKeyboard</a></b></font></td></tr></table>');
+document.write('<table width="100%" border="0" cellspacing="0" cellpadding="1"><tr align="center"><td><font face="Verdana, Geneva, sans serif" size="1"><b><a href="../../vol3/no1/updates.html">up 2 date</a> | <a href="../../vol3/no1/events.html">events</a> | <a href="../../vol3/no1/technook.html">tech nook</a> | <a href="../../vol3/no1/viewpoint.html">view point</a> | <a href="../../archives.html">archives</a> | <a href="../../istp/etcetera8/index.html">etcetera</a> | <a href="../../vol3/no1/links.html">links</a> | <a href="../../vol3/no1/onkeyboard.html">onKeyboard</a></b></font></td></tr></table>');
 
 }
